20963faa00cdc9ffbe57d9215fe3aa15dc590b04
Wahid Hasan <hasanalwahid@gmail.com>
2022-10-05 12:17:00 +0600

    Create enum plan

---
+++ Domain/Enums/Plan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum Plan
+    {
+        [Description("Free")]
+        Free = 50,
+        [Description("Gold")]
+        Gold = 100,
+        [Description("Unlimited")]
+        Unlimited = 500
+    }
+}
